a4831be77ba6836c39eee5b8629db263ed5644a0
Sebastian <sebastian@nielsen2u.dk>
2020-05-17 19:37:01 +0200

    add job

---
+++ vuevue/clientapp/src/components/Manager/AddJob.vue
@@ -0,0 +1,108 @@
+﻿<template>
+    <div id="add-manager">
+        <h1>Add a new Job</h1>
+        <form v-if="!submitted">
+            <label>Customer Name</label>
+            <input type="text" v-model.lazy="job.customer" required />
+            <label>End date:</label>
+            <select v-model="endDate" required>
+                <option v-for="day in endDates" :key="day">{{day}}</option>
+            </select>
+            <label>Password</label>
+            <input type="password" v-model.lazy="job.location" placeholder="Password" required />
+            <label>Password</label>
+            <input type="password" v-model.lazy="job.comments" placeholder="Password" required />
+
+
+            <button class="btn btn-secondary" @click.prevent="PostManager">Add Manager</button>
+        </form>
+
+        <div v-show="submitted">
+            <h3>You have added a new manager</h3>
+        </div>
+        <div id="preview">
+            <h3>Preview Manager</h3>
+            <p>First name {{ manager.firstName }}</p>
+            <p>Last name {{ manager.lastName }}</p>
+            <p>Email {{ manager.email }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+//import moment from 'vue-moment';
+
+export default {
+
+    data(){
+    return{
+        job:{
+            customer: "",
+            startDate: "",
+            days: "",
+            location:"",
+            comments:""
+        },
+        endDates: [1, 2, 7, 14, 28],
+        submitted: false,
+
+
+    }
+    },
+    methods:{
+        PostManager: function () {
+            this.calcEndDate();
+
+
+            axios.post('http://localhost:5000/api/Managers/PostManager',
+                {
+                    FirstName: this.firstName,
+                    LastName: this.lastName,
+                    Email: this.email,
+                    password: this.password
+                })
+            this.submitted = true;
+        },
+        EndDate() {
+            const moment = this.$moment().add(this.endDate, 'days').format();
+            window.console.log(this.EndDate);
+            this.days.expirationDate = moment;
+        },
+    },
+}
+</script>
+
+<style>
+    #add-manager * {
+        box-sizing: border-box;
+    }
+
+    #add-manager {
+        max-width: 800px;
+        min-height: 1000px;
+        margin: 0 auto;
+    }
+
+    label {
+        display: block;
+        margin: 20px 0 5px;
+    }
+
+    input[type="text"], textarea {
+        display: block;
+        width: 100%;
+        padding: 8px;
+    }
+
+    #preview {
+        padding: 10px 20px;
+        border: 1px dotted #ccc;
+        margin: 30px 0;
+    }
+
+    h3 {
+        margin-top: 10px;
+    }
+</style>
