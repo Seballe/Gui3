ef68f41d2954cb390e6be002510b0bb4de5a4ec1
Sebastian <sebastian@nielsen2u.dk>
2020-05-17 20:24:04 +0200

    add job

--- vuevue/clientapp/src/components/Manager/AddJob.vue
+++ vuevue/clientapp/src/components/Manager/AddJob.vue
@@ -1,38 +1,43 @@
 ﻿<template>
-    <div id="add-manager">
+    <div id="add-job">
         <h1>Add a new Job</h1>
         <form v-if="!submitted">
             <label>Customer Name</label>
             <input type="text" v-model.lazy="job.customer" required />
+            <label>Start date:</label>
+            <date-pick v-model="startDate" :format="format"
+                       :parseDate="parseDate" :formatDate="formatDate"
+                       :inputAttributes="{size: 32}"
+                       ></date-pick>
             <label>End date:</label>
             <select v-model="endDate" required>
                 <option v-for="day in endDates" :key="day">{{day}}</option>
             </select>
-            <label>Password</label>
-            <input type="password" v-model.lazy="job.location" placeholder="Password" required />
-            <label>Password</label>
-            <input type="password" v-model.lazy="job.comments" placeholder="Password" required />
+            <label>Location</label>
+            <input type="text" v-model.lazy="job.location" required />
+            <label>Comment</label>
+            <input type="text" v-model.lazy="job.comments" required />
 
-
-            <button class="btn btn-secondary" @click.prevent="PostManager">Add Manager</button>
+            <button class="btn btn-secondary" @click.prevent="PostJob">Add Manager</button>
         </form>
 
         <div v-show="submitted">
             <h3>You have added a new manager</h3>
         </div>
         <div id="preview">
-            <h3>Preview Manager</h3>
-            <p>First name {{ manager.firstName }}</p>
-            <p>Last name {{ manager.lastName }}</p>
-            <p>Email {{ manager.email }}</p>
+            <h3>Preview job</h3>
+            <p>Customer {{ job.customer }}</p>
+            <p>Location {{ job.location }}</p>
+            <p>Comments {{ job.comments }}</p>
         </div>
     </div>
 </template>
 
 <script>
 
-import axios from 'axios';
-//import moment from 'vue-moment';
+    import axios from 'axios';
+    import DatePick from 'vue-date-pick';
+    import fecha from 'fecha';
 
 export default {
 
@@ -40,46 +45,50 @@ export default {
     return{
         job:{
             customer: "",
-            startDate: "",
             days: "",
             location:"",
             comments:""
         },
         endDates: [1, 2, 7, 14, 28],
-        submitted: false,
-
-
+        format: 'dddd MMMM do, YYYY',
+        startDate: fecha.format(new Date(), 'dddd MMMM Do, YYYY'),
+        submitted: false
     }
     },
     methods:{
-        PostManager: function () {
+        PostJob: function () {
             this.calcEndDate();
-
-
             axios.post('http://localhost:5000/api/Managers/PostManager',
                 {
-                    FirstName: this.firstName,
-                    LastName: this.lastName,
-                    Email: this.email,
-                    password: this.password
+                    Customer: this.customer,
+                    StartDate: this.startDate,
+                    Days: this.days,
+                    Location: this.Location,
+                    Comments: this.comments
                 })
             this.submitted = true;
         },
-        EndDate() {
+        calcEndDate() {
             const moment = this.$moment().add(this.endDate, 'days').format();
-            window.console.log(this.EndDate);
-            this.days.expirationDate = moment;
+            window.console.log(this.endDate);
+            this.job.days = moment;
         },
+        parseDate(dateString, format) {
+            return fecha.parse(dateString, format);
+        },
+        formatDate(dateObj, format) {
+            return fecha.format(dateObj, format);
+        }
     },
 }
 </script>
 
 <style>
-    #add-manager * {
+    #add-job * {
         box-sizing: border-box;
     }
 
-    #add-manager {
+    #add-job {
         max-width: 800px;
         min-height: 1000px;
         margin: 0 auto;
